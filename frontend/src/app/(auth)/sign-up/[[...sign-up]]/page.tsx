f094b5545259780ec740c4fb445895e0353ccc25
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2024-12-01 00:46:09 +0530

    feat: redirect user to /dashboard after login or signup

--- frontend/src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ frontend/src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
 	return (
 		<div className="flex items-center justify-center h-screen p-4">
-			<SignUp />
+			<SignUp forceRedirectUrl="/dashboard" />
 		</div>
 	);
 }
