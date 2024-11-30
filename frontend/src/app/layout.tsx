9b7cd8d2ab631dda7152d195d0878c61669eb340
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2024-11-30 16:53:58 +0530

    feat: add navbar

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
 import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
+import { FloatingNav } from "@/components/ui/floating-navbar";
 import { Toaster } from "@/components/ui/sonner";
 import { ClerkProvider } from "@clerk/nextjs";
-
+import { Home, Sparkles, User } from "lucide-react";
 const inter = Inter({
 	subsets: ["latin"],
 	variable: "--font-sans",
@@ -19,6 +20,24 @@ export const metadata: Metadata = {
 	description: "A platform for creating content on steroids",
 };
 
+const navItems = [
+	{
+		name: "Home",
+		link: "/",
+		icon: <Home className="h-4 w-4 text-neutral-500 dark:text-white" />,
+	},
+	{
+		name: "Features",
+		link: "/about",
+		icon: <User className="h-4 w-4 text-neutral-500 dark:text-white" />,
+	},
+	{
+		name: "Try it out",
+		link: "/contact",
+		icon: <Sparkles className="h-4 w-4 text-neutral-500 dark:text-white" />,
+	},
+];
+
 export default function RootLayout({
 	children,
 }: Readonly<{
@@ -30,6 +49,7 @@ export default function RootLayout({
 				<body
 					className={`${inter.variable} ${spaceGrotesk.variable} font-sans antialiased`}
 				>
+					<FloatingNav navItems={navItems} />
 					{children}
 					<Toaster richColors theme="light" />
 				</body>
