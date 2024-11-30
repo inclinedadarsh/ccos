d6c59c204eb4a613d747a375bbdaf4f140501670
Aditya Borase <borse.aditya7@gmail.com>
2024-12-01 02:31:30 +0530

    feat: add functionality to generate blog posts from video transcripts

--- backend/src/contentGenerator/test.ts
+++ backend/src/contentGenerator/test.ts
@@ -1,6 +1,7 @@
 import { getVideoTranscript } from './generateTranscript';
 import { generateTweets } from './generateTweets';
 import { generateLinkedinPosts } from './generateLinkedinPosts';
+import { generateBlogs } from './generateBlogs';
 
 async function test() {
     const videoUrl = "https://www.youtube.com/watch?v=E0EY4dxMAxA";
@@ -9,8 +10,13 @@ async function test() {
         const transcript = transcriptResult.transcript;
         const tweetsResult = await generateTweets(transcript, videoUrl);
         const linkedinPostsResult = await generateLinkedinPosts(transcript, videoUrl);
+        const blogsResult = await generateBlogs(transcript, videoUrl);
+        console.log('Tweets:');
         console.log(tweetsResult);
+        console.log('LinkedIn posts:');
         console.log(linkedinPostsResult);
+        console.log('Blogs:');
+        console.log(blogsResult);
     }
     else {
         console.error('Failed to get video transcript:', transcriptResult.error);
