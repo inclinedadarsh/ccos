6f349771c0ab1672025a77f2cb9013e53f637fb1
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2024-12-01 09:22:22 +0530

    feat: revamp new user component

--- frontend/src/components/NewUser.tsx
+++ frontend/src/components/NewUser.tsx
@@ -8,7 +8,14 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useAtom } from "jotai";
+import { Info } from "lucide-react";
 import { toast } from "sonner";
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger,
+} from "./ui/tooltip";
 
 const NewUser: React.FC = () => {
 	const [webhook1, setWebhook1] = useAtom(discordWebhook1Atom);
@@ -26,48 +33,111 @@ const NewUser: React.FC = () => {
 	};
 
 	return (
-		<div className="flex min-h-screen items-center justify-center w-full">
-			<div className="w-full max-w-md space-y-6 rounded-lg bg-gray-50 border border-neutral-200 p-8 shadow-lg">
-				<h1 className="text-2xl font-bold">Let's get you started!</h1>
+		<TooltipProvider>
+			<div className="flex min-h-screen items-center justify-center w-full">
+				<div className="w-full max-w-md p-8">
+					<h1 className="text-2xl font-bold">
+						Let's get you started!
+					</h1>
+					<p className="text-sm text-neutral-500 mt-4">
+						Tell us your Discord webhook and YouTube channel link so
+						we can send you notifications when your videos are
+						uploaded.
+					</p>
+					<div className="space-y-6 mt-6">
+						<div className="space-y-2">
+							<div className="flex items-center justify-between">
+								<Label htmlFor="webhook1">
+									Discord webhook 1
+								</Label>
+								<Tooltip delayDuration={0}>
+									<TooltipTrigger asChild>
+										<Info
+											size={16}
+											className="cursor-pointer"
+										/>
+									</TooltipTrigger>
+									<TooltipContent>
+										<p className="max-w-xs">
+											Your Discord webhook is the URL that
+											Discord uses to send messages to
+											your server.
+										</p>
+									</TooltipContent>
+								</Tooltip>
+							</div>
+							<Input
+								id="webhook1"
+								type="text"
+								value={webhook1}
+								className="shadow"
+								onChange={e => setWebhook1(e.target.value)}
+							/>
+						</div>
+						<div className="space-y-2">
+							<div className="flex items-center justify-between">
+								<Label htmlFor="webhook2">
+									Discord webhook 2
+								</Label>
+								<Tooltip delayDuration={0}>
+									<TooltipTrigger asChild>
+										<Info
+											size={16}
+											className="cursor-pointer"
+										/>
+									</TooltipTrigger>
+									<TooltipContent>
+										<p className="max-w-xs">
+											Your Discord webhook is the URL that
+											Discord uses to send messages to
+											your server.
+										</p>
+									</TooltipContent>
+								</Tooltip>
+							</div>
+							<Input
+								id="webhook2"
+								type="text"
+								value={webhook2}
+								className="shadow"
+								onChange={e => setWebhook2(e.target.value)}
+							/>
+						</div>
 
-				<div className="space-y-2">
-					<Label htmlFor="webhook1">Discord webhook 1</Label>
-					<Input
-						id="webhook1"
-						type="text"
-						value={webhook1}
-						className="shadow"
-						onChange={e => setWebhook1(e.target.value)}
-					/>
+						<div className="space-y-2">
+							<div className="flex items-center justify-between">
+								<Label htmlFor="youtubeLink">
+									YouTube channel link
+								</Label>
+								<Tooltip delayDuration={0}>
+									<TooltipTrigger asChild>
+										<Info
+											size={16}
+											className="cursor-pointer"
+										/>
+									</TooltipTrigger>
+									<TooltipContent>
+										<p className="max-w-xs">
+											We'll use this to find your videos.
+										</p>
+									</TooltipContent>
+								</Tooltip>
+							</div>
+							<Input
+								id="youtubeLink"
+								type="text"
+								value={youtubeLink}
+								className="shadow text-sm"
+								onChange={e => setYoutubeLink(e.target.value)}
+							/>
+						</div>
+						<Button onClick={handleSubmit} className="w-full">
+							Submit
+						</Button>
+					</div>
 				</div>
-
-				<div className="space-y-2">
-					<Label htmlFor="webhook2">Discord webhook 2</Label>
-					<Input
-						id="webhook2"
-						type="text"
-						value={webhook2}
-						className="shadow"
-						onChange={e => setWebhook2(e.target.value)}
-					/>
-				</div>
-
-				<div className="space-y-2">
-					<Label htmlFor="youtubeLink">YouTube channel link</Label>
-					<Input
-						id="youtubeLink"
-						type="text"
-						value={youtubeLink}
-						className="shadow"
-						onChange={e => setYoutubeLink(e.target.value)}
-					/>
-				</div>
-
-				<Button onClick={handleSubmit} className="w-full">
-					Submit
-				</Button>
 			</div>
-		</div>
+		</TooltipProvider>
 	);
 };
 
